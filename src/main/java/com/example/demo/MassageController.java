61e0c5901ff9a9908948be36a56e345fefb170e8
luoriqiuye <467445743@qq.com>
2023-07-02 09:04:53 +0800

    第一次推送

--- src/main/java/com/example/demo/MassageController.java
+++ src/main/java/com/example/demo/MassageController.java
@@ -1,2 +1,41 @@
-package com.example.demo;public class MassageController {
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+
+@RestController
+public class MessageController {
+
+    private final RedisTemplate<String, Integer> redisTemplate;
+
+    @Autowired
+    public MessageController(RedisTemplate<String, Integer> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/sendMsg/{id}")
+    public String sendMsg(@PathVariable("id") String id) {
+        // 模拟发送短信，将收到的短信数量加1
+        redisTemplate.opsForValue().increment(id, 1);
+        // 设置过期时间为30天
+        redisTemplate.expire(id, Duration.ofDays(30));
+        return "{\"success\":true}";
+    }
+
+    @GetMapping("/record/{id}")
+    public MessageRecord getRecord(@PathVariable("id") String id) {
+        // 查询某个用户当前收到的短信数量
+        Integer count = redisTemplate.opsForValue().get(id);
+        if (count != null) {
+            return new MessageRecord(true, id, "张三", count);
+        } else {
+            return new MessageRecord(false, id, "用户不存在", 0);
+        }
+        //控制每条短信发送间隔
+    }
 }
--- src/main/java/com/example/demo/MassageRecord.java
+++ src/main/java/com/example/demo/MassageRecord.java
@@ -1,2 +1,41 @@
-package com.example.demo;public class MassageRecord {
+package com.example.demo;
+
+class MessageRecord {
+    private String id;
+    boolean success;
+    private String userName;
+    private int count;
+
+    public boolean isSuccess() {
+
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
